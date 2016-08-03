0377f19ae360bc6460888352c3618672a7afde40
Nikhil Surendran <nikhilplr@gmail.com>
2016-08-03 13:39:03 +0530

    search

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,8 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Entity\Giveaway;
+use AppBundle\Form\GiveawayType;
 
 class DefaultController extends Controller
 {
@@ -15,4 +17,48 @@ class DefaultController extends Controller
     {
         return $this->redirectToRoute('fos_user_security_login');
     }
+
+    /**
+     * @Route("/search", name="search")
+     */
+    public function listAction(Request $request)
+    {
+  	
+
+
+
+      $form = $this->get('form.factory')->create(new GiveawayType());
+      if ($this->get('request')->query->has('submit-filter')) { 
+        $form->bind($this->get('request'));
+        
+        // initialize a query builder
+        $filterBuilder = $this->get('doctrine.orm.entity_manager')
+          ->getRepository('AppBundle:Giveaway')
+          ->createQueryBuilder('e');
+
+          $formEntry = $request->get('appbundle_giveaway'); 
+         $filterBuilder->where("e.name like :search")
+		->setParameter("search",  '%'.$formEntry['name'].'%');
+          
+        // build the query from the given form object
+        //$this->get('lexik_form_filter.query_builder_updater')->addFilterConditions($form, $filterBuilder); 
+        $resultQuery = $filterBuilder->getQuery();
+
+        $filteredEntities = $resultQuery->getArrayResult();
+        return $this->render('AppBundle:Giveaway:list.html.twig', array(
+        	'form' => $form->createView(),
+          'giveawaylist' => $filteredEntities,
+          ));
+      }
+
+
+
+      return $this->render('AppBundle:Giveaway:list.html.twig', array(
+        	'form' => $form->createView(),
+        	'giveawaylist' => false,
+        ));
+
+ 
+    }
+
 }
